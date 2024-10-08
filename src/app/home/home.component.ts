ea25d3cd1367f357660e1cb5bbf6b2f3d61a5da5
Shamim Akram <shamm.akram@siemens.com>
2024-10-08 15:46:24 +0530

    initial Commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+goToStudentLogin() {
+throw new Error('Method not implemented.');
+}
+goToAdminLogin() {
+throw new Error('Method not implemented.');
+}
+
+}
