c3802a428ff864194600a272c1e6752c9dbf88a9
SP-msd678 <sagarikapal678@gmail.com>
2024-10-09 17:32:01 +0530

    signup page created

---
+++ src/app/admin/signup/signup.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css']
+})
+export class SignupComponent {
+  signupForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.signupForm = this.fb.group({
+      username: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
+  }
+
+  onSubmit(): void {
+    if (this.signupForm.valid) {
+      const formData = this.signupForm.value;
+      console.log('Form Data:', formData);
+      
+    } else {
+      console.log('Form is invalid');
+    }
+  }
+}
